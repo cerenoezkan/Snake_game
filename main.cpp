5ac8c3c4170b13a864357abd5dace5ff881b6ff3
Yusufpolat4 <yusufpolat294375.0902@gmail.com>
2024-12-23 20:51:51 +0300

    soyut sinifi ekledim ve refactor yaptım

--- main.cpp
+++ main.cpp
@@ -16,8 +16,15 @@ COORD imlecKonumu;
 void kordinat(int x, int y){  // x ve y ekseni kodinatini çizicek
    imlecKonumu.X = x; // x kordinat eksenini temsil eder
    imlecKonumu.Y = y; // y kordinat  eksenini temsil eder
-    SetConsoleCursorPosition(konsol, imlecKonumu); // imleckonumunu konsola atiyor
+   SetConsoleCursorPosition(konsol, imlecKonumu); // imleckonumunu konsola atiyor
 }
+
+// Base class Object
+class Object {   // soyut bir clss tanimladim
+public:
+    virtual void ciz() = 0;  //   çizmek
+    virtual void sil() = 0;   //  silmek
+};
 
 int main() {
 
