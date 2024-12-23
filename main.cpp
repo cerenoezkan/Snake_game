55aa4dad6bf1fc9b0480fce159d010bcfb89a450
Yusufpolat4 <yusufpolat294375.0902@gmail.com>
2024-12-23 20:17:24 +0300

    kutuphaneleri ekledim

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h> /* HANDLE konsol = GetStdHandle(STD_OUTPUT_HAND  COORD imlecKonumu;*/
+#include <time.h>
+
+#define MAX_SNAKE_SIZE 50
+
+int main() {
+
+
+
+    return 0;
+}
