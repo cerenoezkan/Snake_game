5d329fdf356550cb0c423a506c244ea1b8ef0078
cerenoezkan <cer70en@gmail.com>
2024-12-26 12:20:45 +0300

    yorum satirlarini guncelledim

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <conio.h>
-#include <windows.h> /* HANDLE konsol = GetStdHandle(STD_OUTPUT_HAND  COORD imlecKonumu;*/
+#include <windows.h> /* HANDLE konsol = GetStdHandle(STD_OUTPUT_HANDLE); COORD imlecKonumu;*/
 #include <time.h>
 
 #define MAX_SNAKE_SIZE 50
@@ -16,8 +16,16 @@ COORD imlecKonumu;
 void kordinat(int x, int y){  // x ve y ekseni kodinatini çizicek
    imlecKonumu.X = x; // x kordinat eksenini temsil eder
    imlecKonumu.Y = y; // y kordinat  eksenini temsil eder
-    SetConsoleCursorPosition(konsol, imlecKonumu); // imleckonumunu konsola atiyor
+   SetConsoleCursorPosition(konsol, imlecKonumu); // imleckonumunu konsola atiyor
 }
+
+//ANA CLASS YAPISI 
+class Object {   // soyut bir class tanimladim
+public:
+    //Polymorphism kullandık
+    virtual void ciz() = 0;  //çizmek için gerekli metod
+    virtual void sil() = 0;   //silmek için gerekli metod
+};
 
 int main() {
 
